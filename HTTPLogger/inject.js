460cfdf944ed845e8e5eea7b637462f96c96ba70
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2025-05-07 22:59:35 +0700

    Add files via upload

---
+++ HTTPLogger/inject.js
@@ -0,0 +1,36 @@
+(function() {
+    const originalFetch = window.fetch;
+    window.fetch = async function (...args) {
+      const response = await originalFetch.apply(this, args);
+      const clone = response.clone();
+  
+      clone.text().then(body => {
+        chrome.runtime.sendMessage({
+          action: "logResponse",
+          url: clone.url,
+          body: body
+        });
+      });
+  
+      return response;
+    };
+  
+    const originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function (method, url) {
+      this._url = url;
+      return originalOpen.apply(this, arguments);
+    };
+  
+    const originalSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function (body) {
+      this.addEventListener("load", function () {
+        chrome.runtime.sendMessage({
+          action: "logResponse",
+          url: this._url,
+          body: this.responseText
+        });
+      });
+      return originalSend.apply(this, arguments);
+    };
+  })();
+  
